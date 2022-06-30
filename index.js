c9740897b1a0460964bced074c6012ea86fde074
Kate Bennert <kate.bennert@gmail.com>
2022-06-30 10:22:47 -0400

    commit tree e65b40b8348304e30b039d7e780ba4a2962a54b8

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Write your code here!
+let main = document.getElementById('main');
+main.remove();
+
+let newHeader = document.createElement(`h1`);
+newHeader.setAttribute("id", "victory");
+
+
+newHeader.innerHTML = "KATE is the champion";
+document.body.append(newHeader);
+
+console.log(newHeader)
